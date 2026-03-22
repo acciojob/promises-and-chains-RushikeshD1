345348784ae99ba35b7d777dba50f5a4f122a670
RushikeshD1 <141423104+RushikeshD1@users.noreply.github.com>
2026-03-22 16:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("btn").addEventListener('click', (e) => {
 	const name = document.getElementById("name").value.trim();
 
 	if (age==="" || name === ""){
-		alert("Please enter valid details.")
+		alert("Please enter valid details")
 		return
 	}
 
