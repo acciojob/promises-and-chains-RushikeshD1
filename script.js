1194397706b2c9a82754d9b9aefa175e5ac6be05
RushikeshD1 <141423104+RushikeshD1@users.noreply.github.com>
2026-03-22 16:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+//your JS code here. If required.https://www.svgrepo.com/show/345221/three-dots.svg
+
+
+document.getElementById("btn").addEventListener('click', (e) => {
+	e.preventDefault();
+	const age = document.getElementById("age").value;
+	const name = document.getElementById("name").value.trim();
+
+	if (age==="" || name === ""){
+		alert("Please enter valid details.")
+		return
+	}
+
+	const promise = new Promise((resolve, reject)=>{
+		if (age > 18){
+			setTimeout(() => {
+				resolve(`Welcome, ${name}. You can vote.`)
+			}, 4000)
+		}else{
+			setTimeout(() => {
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}, 4000)
+		}
+	})
+
+	promise.then(data => {
+		alert(data)
+	}).catch(err => {
+		alert(err)
+	})
+
+
+	
+})
